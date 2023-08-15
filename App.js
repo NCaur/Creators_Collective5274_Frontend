87c3dd699ad9d0da9b0b1c71f552ad6006d62e8f
demostudent2799 <demostudent2799@gmail.com>
2023-08-15 19:45:24 -0400

    changes

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+import LoginScreen from './screens/LoginScreen';
+import DashboardTabs from './screens/DashboardScreen';
+import SelectMoodScreen from './screens/SelectMoodScreen';
+import MakeJournalScreen from './screens/MakeJournalScreen';
+
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login" headerShown="false">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Dashboard" component={DashboardTabs} options={{ headerShown: false }} />
+        <Stack.Screen name="SelectMood" component={SelectMoodScreen} />
+        <Stack.Screen name="MakeJournal" component={MakeJournalScreen}  />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
